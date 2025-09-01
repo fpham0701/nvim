a1338983d95db626a24e04d1ba727b8aba5daff7
francis <fpham0701@gmail.com>
2025-09-01 13:14:13 -0400

    initial commit

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,67 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    opts = {
+      -- List of servers to ensure are installed by mason-lspconfig
+      inlay_hints = { enabled = true },
+      ---@type lspconfig.options
+      servers = {
+        -- Python
+        pyright = {},
+        ruff_lsp = {},
+
+        -- Rust
+        rust_analyzer = {
+          settings = {
+            ["rust-analyzer"] = {
+              cargo = { allFeatures = true },
+              checkOnSave = {
+                command = "clippy",
+              },
+              diagnostics = {
+                enable = true,
+              },
+            },
+          },
+        },
+
+        -- C / C++
+        clangd = {},
+
+        -- Java
+        jdtls = {},
+
+        -- Web
+        tsserver = {}, -- JavaScript / TypeScript
+        html = {},
+        cssls = {},
+
+        -- Config / data formats
+        jsonls = {},
+        yamlls = {},
+        taplo = {}, -- TOML
+
+        -- Bash
+        bashls = {},
+
+        -- Lua (Neovim config)
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+              },
+              telemetry = { enable = false },
+            },
+          },
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,45 @@
+return {
+  -- Tree-sitter integration
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = {
+        -- Core systems/programming languages
+        "c",
+        "cpp",
+        "rust",
+        "java",
+        "python",
+
+        -- Web / scripting
+        "lua", -- important for Neovim config
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+
+        -- Markup / config
+        "json",
+        "yaml",
+        "toml",
+        "markdown",
+        "markdown_inline",
+
+        -- Shell & tools
+        "bash",
+        "regex",
+        "gitcommit",
+        "gitignore",
+        "dockerfile",
+
+        -- Optional but useful
+        "vim", -- for legacy vimscript
+        "vimdoc",
+        "query", -- for writing Treesitter queries
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+}
